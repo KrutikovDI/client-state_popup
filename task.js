2adc3d27ace88ae88395d95bed5ac3f7a324b525
Danil Krutikov <kdanil.i@yandex.ru>
2024-01-09 18:44:57 +0700

    Initial commit

---
+++ task.js
@@ -0,0 +1,28 @@
+const win = document.getElementById("subscribe-modal");
+const close = document.querySelector(".modal__close_times");
+if (!(getCookie("close") == "True")) {
+  setTimeout(() => {
+    if (!win.classList.contains("modal_active")) {
+      win.classList.add("modal_active");
+    }
+  }, 3000);
+}
+
+close.addEventListener("click", () => {
+  win.classList.remove("modal_active");
+  // document.cookie = "click=True";
+  setCookie("click", "True")
+  console.log(document.cookie);
+});
+
+function setCookie(name, value) {
+  document.cookie = name + "=" + value;
+}
+
+function getCookie(name) {
+  if (document.cookie) {
+    const pairs = document.cookie.split(";");
+    const cookie = pairs.find((p) => p.startsWith(name + "="));
+    return cookie.substr(name.length + 1);
+  }
+}
